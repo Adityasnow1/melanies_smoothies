d5880d23e3069192faadf9faf2b4bc162c66be8f
Adityasnow1 <adityasrivardhan90@gmail.com>
2024-06-20 15:30:58 +0530

    Update streamlit_app.py
    created df

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,8 @@ if ingredients_list:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data = fruityvice_resonse.json(), use_container_width = True)
         
 
 
